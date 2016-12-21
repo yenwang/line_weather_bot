251c90c8c9018d8625bb56734074e29488fc89d7
Yen <f74021145@gmail.com>
2016-12-21 10:33:13 +0800

    line_bot

--- line_bot/views.py
+++ line_bot/views.py
@@ -31,7 +31,7 @@ def callback(request):
                 if isinstance(event.message, TextMessage):
                     lint_bot_api.reply_message(
                         event.reply_token,
-                        TextSendMessage(text=event.message.text)
+                        TextSendMessage(text = 'hi')
                     )
         return HttpResponse()
     else:
